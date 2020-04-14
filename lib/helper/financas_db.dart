8d7c13cce1b16ac84feb12eb056314ecab375369
MateusHBR <mateusmorais.r@gmail.com>
2020-04-14 16:48:48 -0300

    criando instancia do banco + select.

--- lib/helper/financas_db.dart
+++ lib/helper/financas_db.dart
@@ -1,1 +1,63 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+class FinancasHelper {
+  static final String tableName = "date";
+  static final String secondTableName = "gasto";
+
+  static final FinancasHelper _financasHelper = FinancasHelper.internal();
+  Database _db;
+  factory FinancasHelper() {
+    return _financasHelper;
+  }
+
+  FinancasHelper.internal();
+
+  get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initializeDB();
+    return _db;
+  }
+
+  initializeDB() async {
+    final caminhaDB = await getDatabasesPath();
+    final localDB = join(caminhaDB, "db_financas.db");
+
+    var db = await openDatabase(
+      localDB,
+      version: 1,
+      onCreate: _onCreateDB,
+    );
+    return db;
+  }
+
+  _onCreateDB(Database db, int version) async {
+    String sql = "CREATE TABLE $tableName ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "month VARCHAR,"
+        "year VARCHAR,"
+        "sal FLOAT"
+        ")";
+    String sql2 = "CREATE TABLE $secondTableName ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "nome VARCHAR,"
+        "valor FLOAT,"
+        "id_mes INTEGER,"
+        "FOREIGN KEY (id) REFERENCES $tableName(id)"
+        ")";
+    // tabela que irei criar
+    await db.execute(sql);
+    await db.execute(sql2);
+    //executand comando SQL
+  }
+
+  listMonth() async {
+    var bancoDados = await db;
+    String sql = "SELECT * FROM $tableName ORDER BY id DESC";
+    List months = await bancoDados.rawQuery(sql);
+
+    return months;
+  }
+}
