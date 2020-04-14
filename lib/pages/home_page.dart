3c4619325b66b78fb62f7d4b994e2c0c0cf837d4
MateusHBR <mateusmorais.r@gmail.com>
2020-04-14 11:41:16 -0300

    criando tela principal, porem estatica.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,90 @@
+import 'package:despesas_app/pages/mes_financas_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("Minhas finanças"),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: size.height * 0.02,
+          bottom: size.height * 0.01,
+          left: size.height * 0.01,
+          right: size.height * 0.01,
+        ),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MesFinancasPage(),
+              ),
+            );
+          },
+          child: Container(
+            width: size.width,
+            height: size.height * 0.10,
+            child: Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: size.height * 0.02),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Janeiro",
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(
+                              Icons.edit,
+                              color: Colors.green,
+                            ),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.remove_circle,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        // child: ListView.builder(
+        //   itemCount: 1,
+        //   itemBuilder: (context, index) {
+        //     Container(
+        //       width: size.width,
+        //       height: size.height * 0.2,
+        //       child: Card(),
+        //     );
+        //   },
+        // ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
