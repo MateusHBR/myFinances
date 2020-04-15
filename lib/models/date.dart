5e20d8f31cb4a5041dd55a13c328502fcfc9a5a6
MateusHBR <mateusmorais.r@gmail.com>
2020-04-14 22:34:07 -0300

    reconfigurando classe Date, faltavam alguns parâmetros.

--- lib/models/date.dart
+++ lib/models/date.dart
@@ -2,20 +2,23 @@ class Date {
   int id;
   String month;
   String year;
-  double salary;
+  String salary;
 
   Date({
     this.id,
     this.month,
+    this.year,
     this.salary,
   });
 
   Map<String, dynamic> toJson() {
     final json = Map<String, dynamic>();
-    json['id'] = id;
+    // json['id'] = id;
     json['month'] = month;
     json['year'] = year;
     json['salary'] = salary;
+
+    return json;
   }
 
   Date.fromJson(Map<String, dynamic> json) {
