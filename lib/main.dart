883d31bc865d88c8863c05acd73456816b7f5141
MateusHBR <mateusmorais.r@gmail.com>
2020-04-16 16:57:26 -0300

    configurando linguagem PT-BR

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:despesas_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [const Locale('pt', 'BR')],
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
